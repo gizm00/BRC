d8bd9c0811b4faf8eaf7c666f215c3d4103afcb6
Portia Burton <plburton@gmail.com>
2014-11-12 16:12:42 -0800

    Added scraper to RC site

---
+++ scraper/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class rssFeed(models.Model):
+    title = models.CharField(max_length=200)
+    summary = models.CharField(max_length=200
+    link = models.CharField('date published')
+    link = models.CharField(max_length=200)
+
