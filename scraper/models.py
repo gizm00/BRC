4243f9e64729fc4b200b0234ba1548f54eca89a7
Portia Burton <plburton@gmail.com>
2014-11-23 12:06:31 -0800

    Changed name of class

--- scraper/models.py
+++ scraper/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class rssFeed(models.Model):
+class BlogCorpus(models.Model):
     title = models.CharField(max_length=200)
-    summary = models.CharField(max_length=200
+    summary = models.CharField(max_length=200)
     link = models.CharField('date published')
     author = models.CharField(max_length=200)
     category = models.CharField(max_length=200)
