099c4249f85a0f5ea74c47479e15480fcd558955
Portia Burton <plburton@gmail.com>
2014-11-23 12:00:52 -0800

    Added category column in model

--- scraper/models.py
+++ scraper/models.py
@@ -5,5 +5,6 @@ class rssFeed(models.Model):
     title = models.CharField(max_length=200)
     summary = models.CharField(max_length=200
     link = models.CharField('date published')
-    link = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
 
